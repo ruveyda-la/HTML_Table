17436f1fafd5b413977a5bd6db2f4d12a8f890bd
Ruveyda <albayrakruveyda@gmail.com>
2023-01-12 20:55:55 -0800

    first commit

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from users import users
+app = Flask(__name__)
+
+@app.route('/')
+def htmltable():
+    return render_template("index.html", users=users)
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
